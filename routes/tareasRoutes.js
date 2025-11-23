b28c8ff83bb574766a2e6039ca7a05ca711dd1e4
Humberto Cristian Paez <crishpaez1993@gmail.com>
2025-11-23 20:16:39 -0300

    fix de tareasRoudes.js

--- routes/tareasRoutes.js
+++ routes/tareasRoutes.js
@@ -1,15 +1,27 @@
 import express from 'express';
-import { obtenerTareas, mostrarFormulario, crearTarea, editarTarea, actualizarTarea, eliminarTarea } from '../controllers/tareasController.js';
+import { 
+  obtenerTareas, 
+  crearTarea, 
+  editarTarea, 
+  actualizarTarea, 
+  eliminarTarea 
+} from '../controllers/tareasController.js';
 
 const router = express.Router();
 
 router.get('/', obtenerTareas);
+
 router.get('/nuevo', (req, res) => {
-  res.render('tareas/formulario', { hojaEstilo: 'tareas/formulario', tarea: null });
+  res.render('tareas/formulario', { 
+    hojaEstilo: 'tareas/formulario', 
+    tarea: null 
+  });
 });
+
 router.post('/', crearTarea);
 router.get('/editar/:id', editarTarea);
 router.post('/actualizar/:id', actualizarTarea);
-router.post('/eliminar/:id', eliminarTarea);
+//router.post('/eliminar/:id', eliminarTarea);
+router.delete('/eliminar/:id', eliminarTarea);
 
 export default router;
